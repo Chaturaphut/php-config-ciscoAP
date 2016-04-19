9552c16a17995d8471c242911e42f35f2f77a5c7
Chaturaphut Triphet <putter@Chaturaphuts-MacBook-Pro.local>
2016-04-19 20:22:19 +0700

    setting default and change variable

--- config/config.php
+++ config/config.php
@@ -1,12 +1,16 @@
 <?php
 
-  $ap_host = "192.168.1.9";
+  #Access Point Authentication
   $user = "Cisco";
   $pass = "Cisco";
   
   // Config for crontab
   $path = './';
-
+  
+  // Controller IP
+  $controller_host = "192.168.1.1";
+  
+  // Access Point IP
   $ap_list = array();
   $ap_list[] = '192.168.1.2';
   $ap_list[] = '192.168.1.3';
--- crontab.php
+++ crontab.php
@@ -10,7 +10,7 @@
 	file_put_contents($path.'status/cron.pid','1');
 	$check = file_get_contents($path.'status/apStatus.txt');
 	
-	if(pingAddress($ap_host)){
+	if(pingAddress($controller_host)){
 		if($check == '9'){
 			//welcome back AP
 			echo 'Welcome back AP send command Stand Alone to lightweight. '.PHP_EOL;
